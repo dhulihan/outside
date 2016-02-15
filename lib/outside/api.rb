11f9ddd96f7c556202db6802c3b172ca4a9669a6
Dave Hulihan <dhulihan@gmail.com>
2016-02-14 20:48:30 -0700

    Add validation errors

--- lib/outside/api.rb
+++ lib/outside/api.rb
@@ -1,5 +1,13 @@
 require "grape"
 
+class LengthExactly < Grape::Validations::Base
+  def validate_param!(attr_name, params)
+    unless params[attr_name].length == @option
+      fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "must be #{@option} characters long"
+    end
+  end
+end
+
 module Outside
   class API < Grape::API
     format :json
@@ -8,8 +16,15 @@ module Outside
     desc "Get conditions by ZIP code" do
       detail "Please provide a five digit US ZIP code"
     end
+    params do
+      requires :zipcode, type: :string, length_exactly: 5
+    end 
     get :zipcode do
-      { zipcode: params[:zipcode] }
+      zipcode = params[:zipcode]
+      { 
+        zipcode: params[:zipcode],
+        message: "Have a nice day"
+      }
     end
 
     get :hello do
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,43 @@
 $(function() {
+	// disable form
+	$("form").submit(function() {
+		return false;
+	});
+
+	function debug(str){
+		console.log(str)
+	}
+
 	$(document).on("click", "#check", function() {
-		$(this).find(".fa").addClass("fa-spin")
-		$("#results").html("NOPE");
+		var url = "/api/v1/zipcode"; 
+		var zipcode = $("input[name='zipcode']").val();
+
+		$.ajax({
+			url: url,
+			type: "GET",
+			data: {
+				zipcode: zipcode
+			},
+			dataType: "json", 
+			beforeSend: function ( xhr ) {
+				debug("beforeSend")
+				debug("zipcode: " + zipcode)
+				$("a .fa").addClass("fa-spin fa-spinner")
+			},
+			success: function(data, status, xhr){
+				debug("success:")
+				debug(data)
+				$("#results").html(data.message)
+			},
+			error: function(xhr, status, error){
+				var resp = $.parseJSON(xhr.responseText)
+				$("#results").html(resp.error)
+				$("#results").addClass("text-danger")
+			},
+			// Called on success and error
+			complete: function(xhr, status){
+				$("a .fa").removeClass("fa-spin fa-spinner")
+			}
+		});			
 	});
 });	
